e53fd120ed5c29e751eb687bd97c89a5af64c4de
Tristan B. Kildaire <deavmi@disroot.org>
2020-04-16 16:35:07 +0200

    Create server with hardcoded AF_INET4 bind address and TCP port

--- source/app.d
+++ source/app.d
@@ -1,9 +1,13 @@
 module app;
 
 import std.stdio;
-import server.server;
+import server.types;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	/* TODO: Change this */
+	string address = "0.0.0.0";
+	ushort port = 2222;
+
+	BesterServer server = new BesterServer(address, port);
 }
--- source/server/types.d
+++ source/server/types.d
@@ -1,6 +1,14 @@
 module server.types;
 
+import utils.debugging : debugPrint;
+import std.conv : to;
+
 public class BesterServer
 {
+
+	this(string bindAddress, ushort listenPort)
+	{
+		debugPrint("Binding to address: " ~ bindAddress ~ " and port " ~ to!(string)(listenPort));
+	}
 	
 }
