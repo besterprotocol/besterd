59fea8fb20f79089871e8f4ab7b23e9c67f742aa
Tristan B. Kildaire <deavmi@disroot.org>
2020-04-16 16:50:57 +0200

    Added exception handling

--- source/server/server.d
+++ source/server/server.d
@@ -1,9 +1,22 @@
 module server.server;
 
 import server.types;
+import std.conv : to;
+import std.socket : SocketOSException;
+import utils.debugging : debugPrint;
 
 void startServer(string address, ushort port)
 {
-	BesterServer server = new BesterServer(address, port);
-	server.run();
+	BesterServer server = null;
+
+	try
+	{
+		server = new BesterServer(address, port);
+		server.run();
+	}
+	catch(SocketOSException exception)
+	{
+		debugPrint("Error binding to address " ~ address ~ " and port " ~ to!(string)(port));
+	}
+	
 }
