79851163c653860abac3f56d5a05ea2d66760d6d
Tristan B. Kildaire <deavmi@disroot.org>
2020-04-16 17:05:56 +0200

    Added BesterConnection handler

--- source/server/types.d
+++ source/server/types.d
@@ -3,6 +3,7 @@ module server.types;
 import utils.debugging : debugPrint;
 import std.conv : to;
 import std.socket : Socket, AddressFamily, SocketType, ProtocolType, parseAddress;
+import core.thread : Thread;
 
 public class BesterServer
 {
@@ -30,8 +31,36 @@ public class BesterServer
 		debugPrint("Server listen loop started");
 		while(true)
 		{
+			/* Wait for an incoming connection */
+			Socket clientConnection = serverSocket.accept();
+
+			/* TODO: Spawn new BesterConnection object here */
+		}
+	}
+	
+}
+
+private class BesterConnection : Thread
+{
+
+	/* The socket to the client */
+	private Socket clientConnection;
+
+	this(Socket clientConnection)
+	{
+		/* TODO */
+		/* Save socket and set thread worker function pointer */
+		super(&run);
+	}
+
+	/* Read/send loop */
+	private void run()
+	{
+		while(true)
+		{
 			
 		}
 	}
+
 	
 }
