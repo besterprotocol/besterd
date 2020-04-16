9a470a18a5f833da6a569e80e6a34e0ad945fac6
Tristan B. Kildaire <deavmi@disroot.org>
2020-04-16 16:43:00 +0200

    Bind socket and listen

--- source/server/types.d
+++ source/server/types.d
@@ -2,7 +2,7 @@ module server.types;
 
 import utils.debugging : debugPrint;
 import std.conv : to;
-import std.socket : Socket, AddressFamily, SocketType, ProtocolType;
+import std.socket : Socket, AddressFamily, SocketType, ProtocolType, parseAddress;
 
 public class BesterServer
 {
@@ -20,11 +20,13 @@ public class BesterServer
 	{
 		/* Create a socket */
 		serverSocket = new Socket(AddressFamily.INET, SocketType.STREAM, ProtocolType.TCP);
+		serverSocket.bind(parseAddress(bindAddress, listenPort));
 	}
 
 	/* Start listen loop */
 	public void run()
 	{
+		serverSocket.listen(1); /* TODO: This value */
 		debugPrint("Server listen loop started");
 		while(true)
 		{
