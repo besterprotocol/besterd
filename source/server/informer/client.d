a7fb6d42c596887b5dcf9be4cb36155966007185
Tristan B. Kildaire <deavmi@disroot.org>
2020-05-04 15:45:57 +0200

    Added informer handler comunication loop.

--- source/server/informer/client.d
+++ source/server/informer/client.d
@@ -3,7 +3,9 @@ module server.informer.client;
 import core.thread : Thread;
 import server.server : BesterServer;
 import std.socket;
-
+import bmessage;
+import std.json;
+import utils.debugging;
 
 public class BesterInformerClient : Thread
 {
@@ -23,6 +25,37 @@ public class BesterInformerClient : Thread
     private void worker()
     {
         /* TODO: Implement me */
+        while(1)
+        {
+            /* Receive a message */
+            JSONValue handlerCommand;
+            receiveMessage(handlerSocket, handlerCommand);
+
+            /* The response to send */
+            JSONValue handlerResponse;
+
+            /* Attempt to get the JSON */
+            try
+            {
+                /* Get the command type */
+                string commandType = handlerCommand["command"]["type"].str();
+                debugPrint("Command: "~ commandType);
+
+
+
+                /* Set the `status` field to `"0"` */
+                handlerResponse["status"] = "0";
+            }
+            catch(JSONException e)
+            {
+                /* Set the `status` field to `"1"` */
+                handlerResponse["status"] = "1";
+            }
+
+            /* Send the response to the handler */
+            sendMessage(handlerSocket, handlerResponse);
+            
+        }
     }
 
 }
