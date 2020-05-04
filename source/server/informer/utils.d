1fcfaffa28d078b11be275ea99575676cc8610fc
Tristan B. Kildaire <deavmi@disroot.org>
2020-05-04 22:35:07 +0200

    Added `isClient` function.

--- source/server/informer/utils.d
+++ source/server/informer/utils.d
@@ -2,6 +2,7 @@ module server.informer.utils;
 
 import server.server : BesterServer;
 import connection.connection : BesterConnection;
+import std.string : cmp;
 
 /**
 * This functions returns `string[]` where each element
@@ -22,4 +23,23 @@ public static string[] listClients(BesterServer server)
     }
 
     return clientList;
+}
+
+/**
+* This function returns `true` if the provided username
+* matches a locally connected client, `false` otherwise.
+*/
+public static bool isClient(BesterServer server, string username)
+{
+    for(ulong i = 0; i < server.clients.length; i++)
+    {
+        /* Make sure only to match client connections */
+        BesterConnection connection = server.clients[i];
+        if(connection.getType() == BesterConnection.Scope.CLIENT && cmp(connection.getCredentials[0], username))
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
