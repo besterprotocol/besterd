2003505979079a8122b6974ed5178a25513ddf56
Tristan B. Kildaire <deavmi@disroot.org>
2020-05-08 10:32:22 +0200

    Added WIP classes for data store backend.

---
+++ source/server/accounts.d
@@ -0,0 +1,12 @@
+module server.accounts;
+
+import vibe.vibe;
+
+/**
+* This represents the accounts management system of
+* the server. It is only an abstract class.
+*/
+public abstract class BesterDataStore
+{
+
+}
---
+++ source/server/redis.d
@@ -0,0 +1,9 @@
+module server.redis;
+
+import vibe.vibe;
+import server.accounts : BesterDataStore;
+
+public final class RedisDatastore : BesterDataStore
+{
+    
+}
